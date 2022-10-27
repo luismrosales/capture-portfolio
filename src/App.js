ccde8f0d87c1208e91992a71cde8cd110d0b7bbc
Luis Rosales <luismrosales2@gmail.com>
2022-10-26 20:07:44 -0400

    added react router and added content to our work page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// ROUTER
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
