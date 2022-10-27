eb5110ae275323e7e49c58a186969d86aeabd34f
Luis Rosales <luismrosales2@gmail.com>
2022-10-27 00:58:19 -0400

    movie details

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
 // ROUTER
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+
       <Nav />
-      <Routes>
-        <Route path="/" exact element={<AboutUs />} />
-        <Route path="/work" exact element={<OurWork />} />
-        <Route path="/contact" exact element={<ContactUs />} />
-      </Routes>
+
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
